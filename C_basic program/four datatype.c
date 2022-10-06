44dfbfb23ddeb17a9b338a2735fdd7ba5eefb069
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-06 10:16:40 +0530

    Add files via upload

---
+++ C_basic program/four datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+main()
+{
+  int sum=33;
+  float money=44.12;
+  char letter;
+  letter='E';
+  printf("value of sum is %d\n",sum);
+  printf("value of money is %f\n",money);
+  printf("value of letter is %c\n",letter);
+  return 0;
+}
