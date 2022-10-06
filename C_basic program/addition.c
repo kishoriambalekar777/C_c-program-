7694e40661c4e9923170de6733404fc54deff582
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-06 10:08:53 +0530

    Add files via upload

---
+++ C_basic program/addition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int addition,N1=0,N2=0;
+  printf("Enter first number");
+  scanf("%d",&N1);
+  printf("Enter second number");
+  scanf("%d",&N2);
+  addition=N1+N2;
+  printf("\n addition of %d and %d is %d",N1,N2,addition);
+  return 0;
+  
+  
+  
+  
+  
+  
+ 
+}
