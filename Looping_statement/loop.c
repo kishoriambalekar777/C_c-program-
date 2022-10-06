615ab046fa450606d656af0b1d25b4963bfc5821
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-06 16:51:06 +0530

    Add files via upload

---
+++ Looping_statement/loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i=0, j=0, k=1;
+  printf("\n************Welcome************\n");
+  
+  for (i=100;i>97;i--)
+  {
+    printf("\n************GANPATI BAPPA************\n");
+    for (j=1;j<=5;j++)
+    {
+      printf("\n %dkarad",k);
+      k++;
+    }
+    
+    printf("\n \n India \n");
+  }
+    printf("\n\nThank you\n");
+  return 0;
+  
+}
