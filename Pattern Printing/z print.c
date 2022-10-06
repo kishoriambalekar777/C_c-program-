73ddde4610998ba558260d06375b2ead14625bab
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-06 10:41:06 +0530

    Add files via upload

---
+++ Pattern Printing/z print.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int r=0, c=0,k=0;
+  printf("Enter number: ");
+  scanf("%d",&k);
+  for (r=1;r<=k;r++)
+  {
+    for (c=1;c<=k;c++)
+    {
+      if(r==1 || k==r ||c==(k-r+1))
+      
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
