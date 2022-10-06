3553dde008294fb4145f749f4241dc4842eb30bb
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-06 10:05:58 +0530

    Add files via upload

---
+++ C_basic program/Student information.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\n===============**********===============\n");
+  
+  printf("\n\t --Student Information Database--\n");
+  
+  printf("\n%5d %-10s %2d %3d %3d %4d %0.2f %-165",120,"Amitabh",6,95,65,166,55,3333,"Pune");
+  printf("\n%5d %-10s %2d %3d %3d %4d %0.2f %-165",6,"raj",55,45,65,90,60,0.47245,"Kolhapur");
+  printf("\n%5d %-10s %2d %3d %3d %4d %0.2f %-165",4138,"virat",65,95,83,85,66.777245,"Nagpur");
+  printf("\n\n===============**********===============\n");
+  return 0;
+}
