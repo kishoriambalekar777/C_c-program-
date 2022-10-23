c58e20f67325248bcc2593a5e01ad47c9aa088d4
kishoriambalekar777 <103631924+kishoriambalekar777@users.noreply.github.com>
2022-10-23 11:30:12 +0530

    Add files via upload

---
+++ Digit program/Program to find sum of digit in a number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int n,sum=0,r;
+  printf("Enter any number");
+  scanf("%d",&n);
+  while(n>0)
+  {
+    r=n%10;
+    sum=sum+r;
+    n=n/10;
+  }
+  printf("sum of digits:%d",sum);
+  return 0;
+}
